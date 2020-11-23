a600b6372bde149268e62d21398b89a6ad3e2746
Paul Realpe <paulmrg461@gmail.com>
2020-11-23 11:56:33 -0500

    Reto finalizado

--- app.js
+++ app.js
@@ -3,14 +3,26 @@ new Vue({
 
     data() {
         return {
-            
+            courses: [],
+            title: "",
+            time: 0
         }
     },
     computed: {
-        
+        totalTime(){
+            let timer = 0
+            this.courses.forEach(it => {
+                timer += parseInt(it.time) 
+            })
+            return timer
+        }
     },
 
     methods: {
-        
+        addCourse(){
+            this.courses.push({ title: this.title, time: this.time })
+            this.title = ''
+            this.time = 0
+        }
     },
 })
